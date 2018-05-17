0ce0ed8ffc940946d6337803ffa34478a02cc290
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2018-05-17 18:57:32 +0200

    Sync

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,9 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        View::composer(
-            'profile', 'App\Http\ViewComposers\ProfileComposer'
-        );
+        //
     }
 
     /**
